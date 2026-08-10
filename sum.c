783cb865199e143ad4e5f701c1410c1ed70b3a9f
charviverma19 <vermacharvi19@gmail.com>
2026-08-10 23:28:28 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+// Q1: Write a program to input two numbers and display their sum.
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);
+
+    printf("enter b");
+    scanf("%d",&b);
+
+    printf("sum is:%d",a+b);
+    return 0;
+} 
