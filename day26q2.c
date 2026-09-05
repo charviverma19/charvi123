1254d1c555a8977c039bdda20a7231714352ce82
charviverma19 <vermacharvi19@gmail.com>
2026-09-05 14:59:42 +0530

    Create day26q2.c to print asterisk patterns
    Prints asterisks in a specific pattern with new lines.

---
+++ day26q2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    printf("*\n\n");
+
+    
+    for(i = 0; i < 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    for(i = 0; i < 5; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    for(i = 0; i < 3; i++) {
+        printf("*\n");
+    }
+    printf("\n");
+
+    printf("*\n");
+
+    return 0;
+}
