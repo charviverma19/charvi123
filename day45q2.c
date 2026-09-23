2af58573f0595d36f4f18762a6f58725f5e2ead6
Charvi verma <vermacharvi19@gmail.com>
2026-09-23 11:29:51 +0530

    Add case toggling functionality for input string
    This program toggles the case of each character in the input string.

---
+++ day45q2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[200];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isupper(str[i])) {
+            str[i] = tolower(str[i]);
+        } else if (islower(str[i])) {
+            str[i] = toupper(str[i]);
+        }
+    }
+
+    printf("Toggled string: %s\n", str);
+
+    return 0;
+}
